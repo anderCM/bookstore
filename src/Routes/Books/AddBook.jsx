9af57da98e21663a86e02007a4325753fd535554
anderCM <andercm15@gmail.com>
2023-02-27 23:24:56 -0500

    Add book reducer added

--- src/Routes/Books/AddBook.jsx
+++ src/Routes/Books/AddBook.jsx
@@ -1,45 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 
-const AddBook = () => (
-  <div className="container-fluid mt-5">
-    <h4 className="text-muted">ADD NEW BOOK</h4>
-    <form>
-      <div className="row">
-        <div className="col-md-8">
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="title-book"
-              placeholder="Title"
-            />
-            <label htmlFor="title-book">
-              Title
-              {' '}
-              <MenuBookIcon fontSize="small" />
-            </label>
+import { addBook } from '../../redux/books/booksSlice';
+import SimpleToast from '../../Components/Alerts/Toasts';
+
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title || !author) {
+      SimpleToast.fire({
+        icon: 'warning',
+        title: 'Please enter a Title and Author first',
+      });
+      return;
+    }
+    const book = {
+      id: uuidv4(),
+      title,
+      author,
+      genre: 'Not Genre',
+    };
+    dispatch(addBook(book));
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <div className="container-fluid mt-5">
+      <h4 className="text-muted">ADD NEW BOOK</h4>
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-md-7 my-2">
+            <div className="form-floating">
+              <input
+                type="text"
+                className="form-control"
+                id="title-book"
+                placeholder="Title"
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <label htmlFor="title-book">
+                Title
+                {' '}
+                <MenuBookIcon fontSize="small" />
+              </label>
+            </div>
+          </div>
+          <div className="col-md-3 my-2">
+            <div className="form-floating">
+              <input
+                type="text"
+                className="form-control"
+                id="author-book"
+                placeholder="Author"
+                onChange={(e) => setAuthor(e.target.value)}
+              />
+              <label htmlFor="author-book">
+                Author
+                {' '}
+                <LocalLibraryIcon fontSize="small" />
+              </label>
+            </div>
+          </div>
+          <div className="col-md-2 my-2 d-inline-flex">
+            <button type="submit" className="btn btn-lg btn-primary w-100 rounded-0">
+              ADD BOOK
+            </button>
           </div>
         </div>
-        <div className="col-md-4">
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="author-book"
-              placeholder="Author"
-            />
-            <label htmlFor="author-book">
-              Author
-              {' '}
-              <LocalLibraryIcon fontSize="small" />
-            </label>
-          </div>
-        </div>
-      </div>
-    </form>
-  </div>
-);
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -30,7 +30,10 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [...state.books, action.book];
+      return {
+        ...state,
+        books: [...state.books, action.book],
+      };
     case REMOVE_BOOK:
       return { ...state, books: state.books.filter((book) => book.id !== action.id) };
     default:
