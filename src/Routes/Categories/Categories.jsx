485536612800cbcbbeacea052ef5ac7f5ccd16ce
anderCM <andercm15@gmail.com>
2023-02-28 02:56:35 -0500

    Check category status added

--- src/Routes/Categories/Categories.jsx
+++ src/Routes/Categories/Categories.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const Categories = () => (
-  <>
-    <div className="container-fluid my-5">
-      <button type="button" className="btn btn-outline-secondary">Check status</button>
-    </div>
-  </>
-);
+import { statusCategory } from '../../redux/categories/categoriesSlice';
+
+import SimpleToast from '../../Components/Alerts/Toasts';
+
+const Categories = () => {
+  const categoriesStatus = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const handleCheckStatus = () => {
+    dispatch(statusCategory());
+    if (categoriesStatus.status === 'Under Construction') {
+      SimpleToast.fire({
+        title: categoriesStatus.status,
+        icon: 'error',
+      });
+    }
+  };
+
+  return (
+    <>
+      <div className="container-fluid my-5">
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          onClick={handleCheckStatus}
+        >
+          Check status
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Categories;
